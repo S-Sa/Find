d5c0e8e3ba4a63996d85a46699dfcd54c3109ec2
S-Sa <shou@Acv.local>
2015-07-02 01:08:42 +0900

    refs #5 argsの修正
    mainクラスを修正した

--- src/main/java/com/github/finder/main.java
+++ src/main/java/com/github/finder/main.java
@@ -1,11 +1,21 @@
-public Main(String[] arguments){
-    Args args = parseArguments(arguments);
-    
-    Finder finder = new Finder(args);
-    for(String base: args){
-        String[] items = finder.find(base);
-        for(String item: items){
-            System.out.println(item);
+package com.github.finder;
+
+public class Main{
+    public Main(String[] arguments){
+        Args args = parseArguments(arguments);
+    }
+
+    public static void main(String[] args){
+        new Main(args);
+    }
+
+    private Args parseArguments(String[] arguments){
+        Args args = new Args();
+        try {
+            CmdLineParser parser = new CmdLineParser(args);
+            parser.parseArgument(arguments);
+        } catch (CmdLineException e) {
         }
+        return args;
     }
 }
